dc7fe4add5b87260f54730560e9dfef434aa49f0
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-01 18:48:26 +0200

    Move and change app secret to .env

--- backend/app/conf/settings.py
+++ backend/app/conf/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j(a+5lr+j(^2p2nljmn53hd)9!0wf@3mg9ta9^91ucyl&eor&7'
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
