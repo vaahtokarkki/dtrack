bff6598d8667a1ff9593e99e0bd1dd272425b43b
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-03 18:13:05 +0200

    Refactor location state to use ids on devices

--- frontend/src/store/reducers/locationState.js
+++ frontend/src/store/reducers/locationState.js
@@ -9,18 +9,18 @@ export default function(state = initialState, action) {
     switch (action.type) {
       case UPDATE_LOCATION: {
         const { locations } = state
-        const { device, position, accuracy, speed } = action.payload
-        let filteredByDevide = locations.filter(location => location.device === device)
+        const { device, position, accuracy, speed, id } = action.payload
+        let filteredByDevide = locations.filter(location => location.id === id)
         if (filteredByDevide.length) {
-            const updatedDevice = { device, position, accuracy, speed }
-            const notChangedDevices = locations.filter(location => location.device !== device)
+            const updatedDevice = { device, position, accuracy, speed, id }
+            const notChangedDevices = locations.filter(location => location.id !== id)
             return {
                 ...state,
                 locations: notChangedDevices.concat(updatedDevice)
             }
         }
         const oldLocations = [ ...state.locations ]
-        const newLocation = { device, position, accuracy, speed }
+        const newLocation = { device, position, accuracy, speed, id }
         return {
             ...state,
             locations: oldLocations.concat(newLocation)
