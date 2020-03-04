1cd2790cc4995664334aa11c24966e246a0b033e
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-04 11:30:46 +0200

    Add new selectors

--- frontend/src/store/selectors.js
+++ frontend/src/store/selectors.js
@@ -2,13 +2,19 @@ export const getMapState = store => store.mapState
 
 export const getLocationState = store => store.locationState
 
-export const getLocationByDevice = ( store, device ) => {
+export const getLocationByDeviceName = ( store, name ) => {
     const { locations } = store
-    return locations.filter(location => location.device === device)[0] || null
+    return locations.filter(location => location.name === name)[0] || null
 }
+
+export const getLocationByDeviceId = ( store, id ) =>
+    store.locations.filter(location => location.id === id)[0] || null
 
 export const getUserLocation = store =>
     store.locations.filter(device => device.name === "user")[0] || null
+
+export const getDevicesState = store => store.devicesState
+export const getDevices = devicesState => devicesState.devices
 
 export const getSettingsState = store => store.settingsState
 export const getTrackedDevice = store => store.trackedDevice || null
