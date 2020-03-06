fe164cca421b8359fbf31cc8ba1b6ece5af7defc
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-06 19:24:47 +0200

    Add config for url parameter serialization in axios

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
+import Qs from 'qs'
 
 import config from './settings'
 
 const api = axios.create({
-    baseURL: config.apiUrl
+    baseURL: config.apiUrl,
+    paramsSerializer: function (params) {
+        return Qs.stringify(params, {arrayFormat: 'repeat'})
+    },
 })
 
 export default api;
