89db16ee3c23f9262235e1c8a9e22f20564c1344
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-13 17:50:53 +0200

    Refactor list devices with active track view

--- backend/app/tracking/views.py
+++ backend/app/tracking/views.py
@@ -25,7 +25,14 @@ class ListDevicesActiveTrack(generics.GenericAPIView):
         devices = [row["device"] for row in request_data]
         if devices:
             queryset = queryset.filter(pk__in=devices)
-        serializer_context = {"request": request, "data": request_data}
-        return Response(
-            DeviceTrackSerializer(queryset, many=True, context=serializer_context).data
+
+        serializer = DeviceTrackSerializer(
+            queryset, many=True, context=self.get_serializer_context()
         )
+        return Response(serializer.data, status=200)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        context["data"] = json.loads(self.request.body.decode('utf-8'))
+        return context
