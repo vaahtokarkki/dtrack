71e57375a40a7a9d81ccef276cef98826d308635
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-14 21:25:54 +0200

    Add Sentry error reporting on production builds

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import * as Sentry from '@sentry/browser'
 
 import { Provider } from 'react-redux'
 import store from './store/store'
 
-ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
+if (process.env.NODE_ENV === 'production')
+    Sentry.init({dsn: process.env.REACT_APP_SENTRY_DSN})
+
+ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
