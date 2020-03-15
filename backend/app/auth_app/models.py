01f24982174a3b08838a1f1400ccf70d7cb0ca65
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-15 17:33:19 +0200

    Create custom user manager as we don't use username field

--- backend/app/auth_app/models.py
+++ backend/app/auth_app/models.py
@@ -3,12 +3,26 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import UserManager
 
 
+class CustomUserManager(UserManager):
+    def create_user(self, email=None, password=None, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser):
-    email = models.EmailField(blank=True)
+    email = models.EmailField(blank=True, unique=True)
     first_name = models.CharField('first name', max_length=30, blank=True)
     last_name = models.CharField('last name', max_length=30, blank=True)
 
-    objects = UserManager()
+    objects = CustomUserManager()
 
     @property
     def name(self):
