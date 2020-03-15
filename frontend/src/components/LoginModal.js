4651d120790a90bff35f86f75b529bcb97523496
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-15 19:26:31 +0200

    Connect login view to redux

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment } from 'react'
+import { connect } from 'react-redux'
 
 import api from '../utils/api'
+import { fetchUserDetails, updateAccessToken, updateRefreshToken } from '../store/actions'
 
+import Alert from 'react-bootstrap/Alert'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
 
-export const LoginModal = ({visible, toggleModal}) => {
+const LoginModalComponent = props => {
 
     const useInput = ({ type, placeholder }) => {
         const [value, setValue] = useState("")
@@ -22,30 +25,46 @@ export const LoginModal = ({visible, toggleModal}) => {
 
     const [email, emailInput, resetEmailInput] = useInput({ type: "email", placeholder: "Enter email" });
     const [password, passwordInput, resetPasswordInput] = useInput({ type: "password",placeholder: 'Password' });
+    const [error, setError] = useState("")
 
     const submit = async () => {
-        try {
-            const response = await api.post("/token/", { email, password })
-        } catch(error) {
-            console.log('er', error)
-        }
-        resetEmailInput()
-        resetPasswordInput()
+        const response = await api.post("/token/", { email, password })
+
+        if (!response.ok)
+            return setError("Wrong email or password!")
+
+        props.toggleModal()
+        props.updateAccessToken(response.data.access)
+        props.updateRefreshToken(response.data.refresh)
+        props.fetchUserDetails()
+        resetForm()
     }
 
-    const renderForm = () => <Form>
-        <Form.Group controlId="email">
-            <Form.Label>Email</Form.Label>
-            { emailInput }
-        </Form.Group>
+    const resetForm = () => {
+        resetEmailInput()
+        resetPasswordInput()
+        setError("")
+    }
 
-        <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            { passwordInput }
-        </Form.Group>
-    </Form>
+    const renderError = () =>
+        error && <Alert variant={ 'danger' }>{ error }</Alert>
 
-    return <Modal show={ visible } onHide={ toggleModal } animation={false}>
+    const renderForm = () => <Fragment>
+        { renderError() }
+        <Form>
+            <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                { emailInput }
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                { passwordInput }
+            </Form.Group>
+        </Form>
+    </Fragment>
+
+    return <Modal show={ props.visible } onHide={ props.toggleModal } animation={false}>
         <Modal.Header closeButton>
             <Modal.Title>Log in</Modal.Title>
         </Modal.Header>
@@ -54,8 +73,10 @@ export const LoginModal = ({visible, toggleModal}) => {
         </Modal.Body>
 
         <Modal.Footer>
-            <Button variant="secondary" onClick={ toggleModal }>Cancel</Button>
+            <Button variant="secondary" onClick={ props.toggleModal }>Cancel</Button>
             <Button variant="success" onClick={ submit }>Log in</Button>
         </Modal.Footer>
     </Modal>
 }
+
+export const LoginModal = connect(null, { updateAccessToken, updateRefreshToken, fetchUserDetails })(LoginModalComponent)
