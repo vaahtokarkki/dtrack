9226958fbb47635a3f07e2590e2bab49ff44b2a0
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-15 12:42:36 +0200

    Add user fk to device model

--- backend/app/tracking/models.py
+++ backend/app/tracking/models.py
@@ -1,9 +1,13 @@
 from django.contrib.gis.db import models
 
+from auth_app.models import User
+
 
 class Device(models.Model):
     name = models.CharField(max_length=500)
     tracker_id = models.CharField(max_length=500, unique=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='devices',
+                             null=True)
 
     def __str__(self):
         return self.name
