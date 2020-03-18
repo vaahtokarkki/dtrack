833c380fc32ea28e35c5891cefe1fce4120fe55a
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-18 16:10:56 +0200

    Update sentry capture

--- backend/app/conf/middleware.py
+++ backend/app/conf/middleware.py
@@ -1,10 +1,9 @@
 from rest_framework.views import exception_handler
-from sentry_sdk import capture_message
+from raven.contrib.django.raven_compat.models import client
 
 
 def sentry_exception_handler(exc, context):
     response = exception_handler(exc, context)
     if response is not None:
-        if response.status_code == 400:
-            capture_message("Bad Request", level="error")
+        client.captureException()
     return response
