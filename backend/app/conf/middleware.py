93be78cbbce7ac2014684ca623390741486bbc5d
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-20 09:52:56 +0200

    Prevent reporting of http 401 and 404 errors to sentry

--- backend/app/conf/middleware.py
+++ backend/app/conf/middleware.py
@@ -4,6 +4,6 @@ from raven.contrib.django.raven_compat.models import client
 
 def sentry_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    if response is not None:
+    if response is not None and response.status_code not in [401, 404]:
         client.captureException()
     return response
