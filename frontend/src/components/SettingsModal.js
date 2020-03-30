a954fc1305e511b8dbf04c544e95020e5fec063c
Roni Lindholm <roni.lindholm@gmail.com>
2020-03-30 13:24:23 +0300

    Add device settings to settings
    WIP

--- frontend/src/components/SettingsModal.js
+++ frontend/src/components/SettingsModal.js
@@ -1,19 +1,53 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import { connect } from 'react-redux'
+import moment from 'moment'
 
 import api from '../utils/api'
 
 import { updateUserDetails, addNotification } from '../store/actions'
-import { getUserState, getUser } from '../store/selectors'
+import { getUserState, getUser, getDevicesState, getDevices } from '../store/selectors'
 
 import Alert from 'react-bootstrap/Alert'
 import Form from 'react-bootstrap/Form'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import Spinner from 'react-bootstrap/Spinner'
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
+import Table from 'react-bootstrap/Table'
 
 
 const SettingsComponent = props => {
+    const [activeTab, setActiveTab] = useState('user')
+
+    return <Modal show={ props.visible } onHide={ props.toggleModal } >
+        <Modal.Header>
+            <Modal.Title>Settings</Modal.Title>
+        </Modal.Header>
+        <div className="tab-wrapper">
+            <Tabs id="controlled-tab-example"
+                activeKey={ activeTab }
+                onSelect={ (k) => setActiveTab(k) } >
+                <Tab eventKey="user" title="User settings">
+                    <Modal.Title className="modal-subtitle">
+                        User settings
+                    </Modal.Title>
+                    <SettingsForm { ...props } />
+                </Tab>
+                <Tab eventKey="devices" title="Device settings">
+                    <Modal.Title className="modal-subtitle">
+                        Device settings
+                    </Modal.Title>
+                    <DeviceSettings { ...props } />
+                </Tab>
+            </Tabs>
+        </div>
+    </Modal>
+}
+
+
+
+const SettingsForm = props => {
     const useInput = ({ type, initialValue = "" }) => {
         const [value, setValue] = useState(initialValue)
         const input = <Form.Control
@@ -36,7 +70,6 @@ const SettingsComponent = props => {
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
 
-
     const submit = async () => {
         setLoading(true)
         setError("")
@@ -53,47 +86,75 @@ const SettingsComponent = props => {
     const renderError = () =>
         error && <Alert variant={ 'danger' }>{ error }</Alert>
 
-    const renderForm = () => <Fragment>
-        { renderError() }
-        <Form>
-            <Form.Group controlId="firstName">
-                <Form.Label>First name</Form.Label>
-                { firstNameInput }
-            </Form.Group>
-            <Form.Group controlId="lastName">
-                <Form.Label>Last name</Form.Label>
-                { lastNameInput }
-            </Form.Group>
-            <Form.Group controlId="email">
-                <Form.Label>Email (username)</Form.Label>
-                <Form.Control type="text" value={ props.user.email } disabled />
-            </Form.Group>
-            <Form.Group controlId="refreshInterval">
-                <Form.Label>Device refresh interval { refreshInterval } seconds</Form.Label>
-                { refreshIntervalInput }
-            </Form.Group>
-        </Form>
-    </Fragment>
-
-    return<Modal show={ props.visible } onHide={ props.toggleModal } >
-        <Modal.Header closeButton>
-            <Modal.Title>Settings</Modal.Title>
-        </Modal.Header>
+    return loading ? <Spinner animation="border" /> : <Fragment>
         <Modal.Body>
-            { loading ? <Spinner animation="border" /> : renderForm() }
+            { renderError() }
+            <Form>
+                <Form.Group controlId="firstName">
+                    <Form.Label>First name</Form.Label>
+                    { firstNameInput }
+                </Form.Group>
+                <Form.Group controlId="lastName">
+                    <Form.Label>Last name</Form.Label>
+                    { lastNameInput }
+                </Form.Group>
+                <Form.Group controlId="email">
+                    <Form.Label>Email (username)</Form.Label>
+                    <Form.Control type="text" value={ props.user.email } disabled />
+                </Form.Group>
+                <Form.Group controlId="refreshInterval">
+                    <Form.Label>Device refresh interval { refreshInterval } seconds</Form.Label>
+                    { refreshIntervalInput }
+                </Form.Group>
+            </Form>
         </Modal.Body>
-
         <Modal.Footer>
             <Button variant="secondary" onClick={ props.toggleModal } disabled={ loading }>Cancel</Button>
             <Button variant="success" onClick={ submit } disabled={ loading }>Save</Button>
         </Modal.Footer>
-    </Modal>
+    </Fragment>
+}
+
+const DeviceSettings = props => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
+
+    const renderDevices = () =>
+        props.devices.map(device => <tr key={ device.id }>
+            <td>{ device.name }</td>
+            <td>{ moment(device.last_seen).format("DD.MM.YYYY") }</td>
+            <td>{ device.tracker_id }</td>
+        </tr>)
+
+    const submit = async () => {
+        setLoading(true)
+        setError("")
+    }
+
+    return <Fragment>
+        <Modal.Body>
+            <Table>
+                <thead>
+                    <tr><td>Device</td><td>Last seen</td><td>Tracker id</td></tr>
+                </thead>
+                <tbody>
+                    { renderDevices() }
+                </tbody>
+            </Table>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={ props.toggleModal } disabled={ loading }>Cancel</Button>
+            <Button variant="success" onClick={ submit } disabled={ loading }>Save</Button>
+        </Modal.Footer>
+    </Fragment>
 }
 
 const mapStateToProps = state => {
     const userState = getUserState(state)
+    const devicesState = getDevicesState(state)
+    const devices = getDevices(devicesState).filter(d => d.id !== "user")
     const user = getUser(userState)
-    return { user }
+    return { user, devices }
 }
 
 export default connect(mapStateToProps, { updateUserDetails, addNotification })(SettingsComponent)
