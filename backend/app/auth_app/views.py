51f5888294f8a2d685aee03511ffbf5e49c2f4fe
Roni Lindholm <roni.lindholm@gmail.com>
2020-04-27 21:32:52 +0300

    Add permissions for user details view

--- backend/app/auth_app/views.py
+++ backend/app/auth_app/views.py
@@ -2,12 +2,14 @@ from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import User
+from .permissions import CanAccessUser
 from .serializers import MyTokenObtainPairSerializer, UserSerializer
 
 
 class UserDetailsView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (CanAccessUser,)
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
