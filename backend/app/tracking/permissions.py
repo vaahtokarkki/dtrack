d5e358b4d02a6589eccbbf57bef37b61c13f559c
Roni Lindholm <roni.lindholm@gmail.com>
2020-05-08 12:15:31 +0300

    Fix app crash on permissions

--- backend/app/tracking/permissions.py
+++ backend/app/tracking/permissions.py
@@ -5,11 +5,15 @@ from .models import Device
 
 class HasAccessToDevice(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
         return obj in request.user.devices.all()
 
 
 class HasAccessToTrack(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if not request.user.devices:
+            return False
         user_devices = Device.objects \
             .filter(pk__in=request.user.devices.values_list("pk", flat=True))
         return obj.device in user_devices
